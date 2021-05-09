a7bd1f5e862d34889e3f838e4ed1770fcf46d487
JonathanSignorino <jonisignorino@gmail.com>
2021-05-08 23:02:39 -0300

    JS subida al repo

---
+++ Clase1/clase1.js
@@ -0,0 +1,14 @@
+// Script para que solicita Nombre y apellido y a su vez lo imprime como alerta y en console.log
+var primerNombre = prompt("Ingrese su nombre");
+var apellido = prompt("Ingrese su apellido");
+var nombreCompleto = primerNombre + " " + apellido;
+alert ("Hola!" + " " + nombreCompleto + " " + "Como estas?");
+console.log("Hola!" + " " + nombreCompleto + " " + "Como estas?");
+
+// Script que solicita dos edades para luego imprimir la suma de las mismas
+let primeraEdad = parseInt(prompt("Ingresa tu edad:"));
+let segundaEdad = parseInt(prompt("Ingresa la edad de tu pareja"));
+let sumaEdades = (primeraEdad + segundaEdad);
+alert ("La suma de ambas edades da un total de" + " " + sumaEdades)
+
+
---
+++ Clase2/clase2.js
@@ -0,0 +1,11 @@
+//Script que solicita años de experiencia trabajando para determinar grado de seniority
+var seniority = parseInt(prompt("Ingresa tus años de experiencia trabajando:"));
+if (seniority <= 3) {
+    alert("Tu rango de seniority actual es Junior(Jr)");
+}
+else if (seniority <= 5) {
+    alert("Tu rango de seniority actual es Semi Senior(Ssr)");
+}
+else if (edad > 6) {
+    alert("Tu rango de seniority actual es Senior(Sr)");
+}
